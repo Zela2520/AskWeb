e87afa7610f52ca27479d16b66edcb3152991af5
Zela2520 <sugarbeatzela@gmail.com>
2024-10-28 17:12:08 +0300

    fix getComments

--- ui/src/widgets/CommentsList/ui/CommentsList.vue
+++ ui/src/widgets/CommentsList/ui/CommentsList.vue
@@ -59,7 +59,7 @@ export default defineComponent({
 
         onMounted(() => {
             checkReload();
-            if (!commentsList.value.length) getComments(commentId);
+            getComments(commentId);
         });
 
         return {
